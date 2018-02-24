3fc6e2393d49d9715bda90c43b7137385593ff8b
Gumichan01 <luxon.jean.pierre@gmail.com>
2018-02-24 18:13:44 +0100

    typage: project

--- typage/project/Conversion.ml
+++ typage/project/Conversion.ml
@@ -7,20 +7,49 @@
   Author : Luxon JEAN-PIERRE
 *)
 
-(* Import module ChurchType *)
+open ChurchType;;
 
 (*
-  A substitution is just a pair <old_name, new_name> tha represents {x / y}
+  A substitution is just a pair <old_name, new_name> that represents {x / y}
   *)
-type substitution = string * string
+type substitution = string * string;;
 
-type subList = substitution list
+type subList = substitution list;;
+
+
+let assoc_if a l =
+  try
+      Some(List.assoc a l)
+  with
+  | _ -> None
 
 (*
-  TODO
+  todo
   It operates the alpha-conversion by taking a church type and a substitution list
   to apply
 
-  @note The function fails if a variable capture happen during the alpha-conversion
+  @note The function fails if a variable capture happened during the alpha-conversion
 *)
-let rec alphaconv : unit -> subList -> unit = ()
+let rec alpha_conv e env : chexpression =
+  match e with
+  | Var(s) ->
+    let ns = subs_var s env in
+    Var(ns)
+
+  | Pair(e1, e2) -> Pair((alpha_conv e1 env), (alpha_conv e2 env))
+
+  | Apply(e1, e2) -> Apply((alpha_conv e1 env), (alpha_conv e2 env))
+
+  | _ -> failwith "TODO the rest"
+
+  and subs_var s env =
+    match assoc_if s env with
+    | None      -> s
+    | Some(sub) -> sub
+;;
+
+
+let che = Pair ( Var("x") , Pair( Var("z"), Var("x") ) ) in
+let env = ("x","y") :: ("z","w") :: [] in
+let res = alpha_conv che env in
+pretty_print_e res; print_endline("");;
